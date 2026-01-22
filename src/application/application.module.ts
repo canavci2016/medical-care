397b129b22c45f3d998f9d3ac4960072baf830e5
can avci <canavci2016@gmail.com>
2026-01-22 20:54:49 +0300

    start point

---
+++ src/application/application.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import { ApplicationController } from './application.controller';
+import { ApplicationService } from './application.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT', 5432),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: configService.get('DB_SYNC_SCHEMA') === 'true',
+        extra: {
+          ssl:
+            configService.get('DB_SSL') === 'true'
+              ? { rejectUnauthorized: false }
+              : undefined,
+        },
+        logging: configService.get('NODE_ENV') === 'local',
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+
+  controllers: [ApplicationController],
+  providers: [ApplicationService],
+})
+export class ApplicationModule { }
